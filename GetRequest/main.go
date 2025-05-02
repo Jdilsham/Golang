d8c97a8dc2fa15bbe0a4bb9f52fbb337ff0c8134
janitha2222 <janithadilsham@gmail.com>
2025-05-02 20:35:27 +0530

    get req

---
+++ GetRequest/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Get Request in golang")
+
+	performGetRequest()
+}
+
+func performGetRequest() {
+	const myurl = "https://www.google.com"
+
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	fmt.Println("Response Status: ", response.Status)
+	fmt.Println("Content Length: ", response.ContentLength)
+
+	var responseString strings.Builder
+	content, err := ioutil.ReadAll(response.Body)
+	byteCount, _ := responseString.Write(content)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Byte Count: ", byteCount)
+	fmt.Println("Response Body: ", responseString.String())
+	//fmt.Println("Response Body: ", string(content))1.
+	//    - `https://www.google.com`
+
+}
