60a17977fc429d880aa3ebbfd6a6b426b27d94a8
janitha2222 <janithadilsham@gmail.com>
2025-05-06 21:31:36 +0530

    API

--- API/main.go
+++ API/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
 
 // Model for course - file
 type Course struct {
@@ -21,10 +30,145 @@ var courses []Course
 //middleware, helper - file
 
 func IsEmpty(c *Course) bool {
-	return c.CourseId == "" && c.CourseName == ""
+	//return c.CourseId == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 
 func main() {
 	fmt.Println("Welcome to API")
 
+	r := mux.NewRouter()
+
+	//seeding
+	courses = append(courses, Course{
+		CourseId:    "1",
+		CourseName:  "Go",
+		CoursePrice: 100,
+		Author: &Author{
+			FullName: "Janitha",
+			Website:  "https://www.google.com",
+		},
+	})
+
+	courses = append(courses, Course{
+		CourseId:    "2",
+		CourseName:  "React",
+		CoursePrice: 200,
+		Author: &Author{
+			FullName: "Janitha",
+			Website:  "https://www.google.com",
+		},
+	})
+
+	//routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/courses/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/courses", createOneCourse).Methods("POST")
+	r.HandleFunc("/courses/{id}", updateOneCourse).Methods("PUT")
+	r.HandleFunc("/courses/{id}", deleteOneCourse).Methods("DELETE")
+
+	//listen to a port
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+//controllers - file
+
+//serve home route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to API by Janitha</h1>"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Get All courses")
+	w.Header().Set("Contennt-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Get One course")
+	w.Header().Set("Contennt-Type", "application/json")
+
+	//grab id from request
+	params := mux.Vars(r)
+
+	//loop through courses, find matching id and return the response
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No course found with given id")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Create One course")
+	w.Header().Set("Contennt-Type", "application/json")
+
+	// what if: body is empty
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please provide a body")
+	}
+
+	// what about - {}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if IsEmpty(&course) {
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return
+	}
+
+	//generate unique id, and convert to string
+	//append course into courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
+}
+
+func updateOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Update One course")
+	w.Header().Set("Contennt-Type", "application/json")
+
+	// first - grab id from request
+	params := mux.Vars(r)
+
+	// loop, id, remove, add with my ID
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No course found with given id")
+}
+
+func deleteOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Delete One course")
+	w.Header().Set("Contennt-Type", "application/json")
+
+	params := mux.Vars(r)
+
+	//loop, id, remove
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode("No course found with given id")
 }
